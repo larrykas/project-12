cdde793cca103bc647222645b4bbd98a11687115
larrykas <larrykas@users.noreply.github.com>
2013-11-05 06:46:26 +0300

    Update insert_prepared_stmt.rb

--- insert_prepared_stmt.rb
+++ insert_prepared_stmt.rb
@@ -4,18 +4,14 @@ require 'mysql'
 
 begin
      # connect to the MySQL server
-     con = Mysql.new 'localhost', 'Larry', '**********'
+     con = Mysql.new 'localhost', 'root', '**********', 'banking_system'
      sth = con.prepare( "INSERT INTO employees(id, name, job, gender)VALUES (?, ?, ?, ?)" )
      sth.execute(4, 'Larry', 'Programmer', 'Male')
      sth.execute(5, 'Sedy', 'Programmer', 'Male')
-     sth.finish
      con.commit
      puts "Record has been created"
 rescue Exception => e
-     puts "e"
+     puts e
      puts e.backtrace.join('\n')
      
-ensure
-     # disconnects from server
-     con.disconnect if con
 end
