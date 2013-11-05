af14811d688b12051488c45bf1c455c3b7a1ed82
larrykas <larrykas@users.noreply.github.com>
2013-11-05 05:33:31 +0300

    Create insert_prepared_stmt.rb

---
+++ insert_prepared_stmt.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+require 'mysql'
+
+begin
+     # connect to the MySQL server
+     con = Mysql.new 'localhost', 'Larry', '**********'
+     sth = con.prepare( "INSERT INTO employees(id, name, job, gender)VALUES (?, ?, ?, ?)" )
+     sth.execute(4, 'Larry', 'Programmer', 'Male')
+     sth.execute(5, 'Sedy', 'Programmer', 'Male')
+     sth.finish
+     con.commit
+     puts "Record has been created"
+rescue Exception => e
+     puts "e"
+     puts "e.backtrace.join('\n')"
+     
+ensure
+     # disconnects from server
+     con.disconnect if con
+end
